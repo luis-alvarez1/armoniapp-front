c25d1c627d9c355d7703b19e0a833ac88629b06b
David-Cabeen <mediatecnicadavidcabeen@gmail.com>
2024-06-26 10:24:52 -0500

    Se implementa Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+export default function App() {
 
   return (
     <>
+      <div className='d-flex justify-content-center align-text-center bg-dark vh-100'>
+        <h1 className='bg-primary text-white rounded py-3 px-5 my-auto'>Bootstrap Funcionando</h1>
+      </div>
     </>
   )
 }
-
-export default App
